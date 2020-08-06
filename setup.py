72e12b411b6f3ede16635b693c4f91b1ff624bfc
All-Light <66020638+All-Light@users.noreply.github.com>
2020-08-06 14:18:31 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="PygameEase",
+	version="0.1",
+	description="A simple plugin for buttons, prompts and images",
+	url="",
+	author="All Light",
+	author_email="Alexander1868@hotmail.com",
+	packages=["PygameEase"],
+	zip_safe=False)
